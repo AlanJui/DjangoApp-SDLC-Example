a9a2665d48d630bbe68b70ec9f459ae9e08a3c51
Alan Jui <alanjui.1960@gmail.com>
2021-04-11 15:01:38 +0800

    啟用 pyright

--- app_config/settings/__init__.py
+++ app_config/settings/__init__.py
@@ -3,7 +3,9 @@ import os
 from .base import *
 
 
-from django.core.exceptions import ImproperlyConfigured
+#  from django.core.exceptions import ImproperlyConfigured
+#
+#
 #  def get_env_value(env_variable):
 #      try:
 #          return os.environ[env_variable]
